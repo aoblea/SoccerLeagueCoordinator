47cc6374f60ca4ba0a6a51edc9cdc55f5c6e35a2
arwin oblea <arwinoblea@gmail.com>
2019-05-02 23:43:54 -0400

    Created a collection. Then entered player data manually into the collection

---
+++ soccer-league-coordinator.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Manually entered player data into a collection
+let player1: [String : String] = ["name": "Joe Smith", "height": "42", "experience": "yes", "guardians": "Jim and Jan Smith"]
+let player2: [String : String] = ["name": "Jill Tanner", "height": "36", "experience": "yes", "guardians": "Clara Tanner"]
+let player3: [String : String] = ["name": "Bill Bon", "height": "43", "experience": "yes", "guardians": "Sara and Jenny Bon"]
+let player4: [String : String] = ["name": "Eva Gordon", "height": "45", "experience": "no", "guardians": "Wendy and Mike Gordon"]
+let player5: [String : String] = ["name": "Matt Gill", "height": "40", "experience": "no", "guardians": "Charles and Sylvia Gill"]
+let player6: [String : String] = ["name": "Kimmy Stein", "height": "41", "experience": "no", "guardians": "Bill and Hillary Stein"]
+let player7: [String : String] = ["name": "Sammy Adams", "height": "45", "experience": "no", "guardians": "Jeff Adams"]
+let player8: [String : String] = ["name": "Karl Saygan", "height": "42", "experience": "yes", "guardians": "Heather Bledsoe"]
+let player9: [String : String] = ["name": "Suzane Greenberg", "height": "44", "experience": "yes", "guardians": "Henrietta Dumas"]
+let player10: [String : String] = ["name": "Sal Dali", "height": "41", "experience": "no", "guardians": "Gala Dali"]
+let player11: [String : String] = ["name": "Joe Kavalier", "height": "39", "experience": "no", "guardians": "Sam and Elaine Kavalier"]
+let player12: [String : String] = ["name": "Ben Finkelstein", "height": "44", "experience": "no", "guardians": "Aaron and Jill Finkelstein"]
+let player13: [String : String] = ["name": "Diego Soto", "height": "41", "experience": "yes", "guardians": "Robin and Sarika Soto"]
+let player14: [String : String] = ["name": "Chloe Alaska", "height": "47", "experience": "no", "guardians": "David and Jamie Alaska"]
+let player15: [String : String] = ["name": "Arnold Willis", "height": "43", "experience": "no", "guardians": "Claire Willis"]
+let player16: [String : String] = ["name": "Phillip Helm", "height": "44", "experience": "yes", "guardians": "Thomas Helm and Eva Jones"]
+let player17: [String : String] = ["name": "Les Clay", "height": "42", "experience": "yes", "guardians": "Wynonna Brown"]
+let player18: [String : String] = ["name": "Herschel Krustofski", "height": "45", "experience": "yes", "guardians": "Hyman and Rachel Krustofski"]
+
